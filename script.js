5b7b5c7c7219192408c703d081f29e4d15c1bb04
Fawzan <fauzdasoodais@gmail.com>
2023-01-15 17:37:11 +0300

    latest updates

---
+++ script.js
@@ -0,0 +1,7 @@
+let img = document.getElementsByTagName('.img');
+img.addEventListener("click" , function(){
+  let more = document.create("p")
+  p.document.appendChild(document.createTextNode("contact us for more")
+   
+
+})
